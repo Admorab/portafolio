012b2d092173746bdf2f6af6d66051e54724c3dc
Ricardo Bolaños <ricardo.1701617114@ucaldas.edu.co>
2021-05-17 21:48:05 -0500

    Dark-Light v1
    Se agregó el tema light y la funcionalidad para cambiar de temas, además de corregir la versión responsiva de tablet y agregar los iconos de navegación

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,17 +3,45 @@ import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 import { MatSidenav } from '@angular/material/sidenav';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+
+const lightNav = { backgroundColor: '#b7b7a4', color: 'black' };
+const darkNav = { color: 'white', };
+const lightBody = { backgroundColor: '#6e6e68', color: 'black' };
+const darkBody = {  color: 'white' };
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('temaNav', [
+      state('light', style(lightNav)),
+      state('dark', style(darkNav)),
+      transition('light <=> dark', animate(500)),
+    ]),
+    trigger('temaBody', [
+      state('light', style(lightBody)),
+      state('dark', style(darkBody)),
+      transition('light <=> dark', animate(500)),
+    ]),
+  ],
 })
 export class AppComponent {
   title = 'portafolio';
+  temaNav = 'dark';
+  temaBody = 'dark';
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
     .pipe(
-      map(result => result.matches),
+      map((result) => result.matches),
       shareReplay()
     );
 
@@ -21,8 +49,8 @@ export class AppComponent {
 
   selected = 'Inicio';
 
-  cerrarSide(sidenav:MatSidenav, selected: string) {
-    sidenav.toggle();
-    this.selected = selected;
+  cambiarTema() {
+    this.temaNav = this.temaNav === 'dark' ? 'light':'dark';
+    this.temaBody = this.temaBody === 'dark' ? 'light':'dark';
   }
 }
